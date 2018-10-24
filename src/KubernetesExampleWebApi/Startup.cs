5a7368e92f356f13adf23c9ba0a8f493686cda8c
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-10-24 11:52:58 +0200

    always use UseDeveloperExceptionPage

--- src/KubernetesExampleWebApi/Startup.cs
+++ src/KubernetesExampleWebApi/Startup.cs
@@ -39,9 +39,11 @@ namespace KubernetesExampleWebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
             }
             else
             {
